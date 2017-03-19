f948eb922edbe4f2735e6e1366b5a1b89062d6a9
Carole <cjpillette@hotmail.com>
2017-03-20 09:00:24 +1100

    implemented routing for my 6 static pages and their templates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,23 @@ class PagesController < ApplicationController
   def index
   end
 
-  def show
+  def more_about_me
   end
+
+  def quarantine_project
+  end
+
+  def travel_project
+  end
+
+  def cfa_scholarship_project
+  end
+
+  def chat_room_project
+  end
+
+  def pure_ruby_project
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+
+  get 'pages/more_about_me'
+  get 'pages/quarantine_project'
+  get 'pages/cfa_scholarship'
+  get 'pages/travel_project'
+  get 'pages/chat_room_project'
+  get 'pages/pure_ruby_project'
+
   root 'pages#index'
-  resources :pages
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
