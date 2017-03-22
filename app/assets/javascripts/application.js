70b78eff127372c308f2223caca08734e8b29f59
Carole <cjpillette@hotmail.com>
2017-03-22 16:38:02 +1100

    added jquery, smooth scrolling and offset for fixed navbar in my js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,27 @@
 //= require turbolinks
 //= require_tree .
 
+$(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+    var nav = $('#navigation').height();
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $id.offset().top - nav;
+
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos});
+});
+
 $(window).scroll(function(){
   var wScroll = $(this).scrollTop();
   console.log(wScroll);
